f39d454381fc45509024a9a3148fb66ce322c66e
kayacahit <cahit.kaya@elium.academy>
2017-11-16 11:46:58 +0100

    added google places autocomplete input to map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Map from './components/Map';
+import MapAddressInput from './components/MapAddressInput';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Map />
+      <div className="map-container">
+      <div className='map-addresses-input'>
+          <MapAddressInput />
+          <MapAddressInput />
+      </div>
+        <Map className="map" />
       </div>
     );
   }
