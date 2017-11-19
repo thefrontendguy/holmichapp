3f4d2bb781c029fe44561255b67c7fac9fc0478c
Radovan Delic <infamouszgbgd@yahoo.com>
2017-11-19 17:08:16 +0100

    password encryption

--- server/index.js
+++ server/index.js
@@ -1,13 +1,39 @@
 var express = require("express");
 var app = express();
 var userRoutes = require("./users");
-var compress = require('compression');
+var compress = require("compression");
+var secret = require("./config/keys").sessionsecret;
 app.use(compress());
 
+// set up passport
+/* var passport = require('passport');
+var expressSession = require('express-session');
+app.use(expressSession({ secret: secret }));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+});
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+}); */
+
+// set up session
+var session = require('express-session');
+app.use(session({
+    secret: secret,
+    resave: true,
+    saveUninitialized: false
+}));
+
+// set up body-parser
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// set up cors
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
--- server/messages/schema.js
+++ server/messages/schema.js
@@ -1,3 +1,5 @@
+var bcrypt = require('bcrypt');
+
 var schema = {
     user1: { // ID
         type: String,
@@ -18,6 +20,37 @@ var schema = {
             required: true
         }
     }]
+}
+
+schema.pre('create', next => {
+    var user = this;
+    bcrypt.hash(user.password, 8, (err, hash) => {
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
+});
+
+schema.statics.authenticate = (email, password, callback) => {
+    User.findOne({ email: email })
+        .exec((err, user) => {
+            if (err) {
+                return callback(err)
+            } else if (!user) {
+                var err = new Error('User not found.');
+                err.status = 401;
+                return callback(err);
+            }
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (result === true) {
+                    return callback(null, user);
+                } else {
+                    return callback();
+                }
+            })
+        });
 }
 
 module.exports = schema;
